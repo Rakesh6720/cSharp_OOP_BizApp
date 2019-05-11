df50812e6995ff7923bd64647f6f906f8daf692d
Rakesh Baruah <rbaruah@gmail.com>
2019-05-11 00:26:19 -0500

    Product class updated w/ nullable type for decimal value currentPrice.

--- ACM.BL/Product.cs
+++ ACM.BL/Product.cs
@@ -16,17 +16,15 @@ namespace ACM.BL
         }
 
         public int ProductId { get; private set; }
-
         public string ProductName { get; set; }
-        public string Description { get; set; }
-        public double CurrentPrice { get; set; }
+        public string ProductDescription { get; set; }
+        public decimal? CurrentPrice { get; set; }
 
         public bool Validate()
         {
             bool isValid = true;
             if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
-            if (string.IsNullOrWhiteSpace(Description)) isValid = false;
-            if (double.IsNaN(CurrentPrice)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
 
             return isValid;
         }
