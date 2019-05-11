f12f12d0aa8a2736558d6d3f273388cc3f7f90b1
Rakesh Baruah <rbaruah@gmail.com>
2019-05-11 01:27:29 -0500

    product repository and unit test class created.

--- ACM.BL/ProductRepository.cs
+++ ACM.BL/ProductRepository.cs
@@ -4,7 +4,20 @@ using System.Text;
 
 namespace ACM.BL
 {
-    class ProductRepository
+    public class ProductRepository
     {
+        public Product Retrieve(int productId)
+        {
+            var product = new Product(productId);
+
+            if(productId == 1)
+            {
+                product.CurrentPrice = (decimal)9.99;
+                product.ProductName = "Garden Hose";
+                product.ProductDescription = "A plastic hose perfect for the outdoor patio or lawn.";
+            }
+
+            return product;
+        }
     }
 }
---
+++ Tests/ACM.BLTest/ProductRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class ProductRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            //-- Arrange
+            var productRepository = new ProductRepository();
+            var expected = new Product(1)
+            {
+                CurrentPrice = (decimal)9.99,
+                ProductName = "Garden Hose",
+                ProductDescription = "A plastic hose perfect for the outdoor patio or lawn."
+            };
+
+            //-- Act
+            var actual = productRepository.Retrieve(1);
+
+            //-- Assert
+            Assert.AreEqual(expected.CurrentPrice, actual.CurrentPrice);
+            Assert.AreEqual(expected.ProductDescription, actual.ProductDescription);
+            Assert.AreEqual(expected.ProductName, actual.ProductName);
+        }
+    }
+}
