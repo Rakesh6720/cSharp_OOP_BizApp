6df1ba6a285807ae65a4c34033aa1e8086726ba7
Rakesh Baruah <rbaruah@gmail.com>
2019-05-11 00:23:22 -0500

    Added Product, Order, and OrderItem classes.

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -4,7 +4,32 @@ using System.Text;
 
 namespace ACM.BL
 {
-    class Order
+    public class Order
     {
+        public int OrderId {get; private set;}
+        public DateTime OrderDate { get; set; }
+
+        public bool Validate()
+        {
+            var isValid = true;
+            
+            return isValid;
+        }
+
+        public Order Retrieve(int orderId)
+        {
+
+            return new Order();
+        }
+
+        public List<Order> Retrieve()
+        {
+            return new List<Order>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
     }
 }
---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACM.BL
+{
+    class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+        public OrderItem(int orderItemId)
+        {
+            OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public Product Product { get; set; }
+        public int Quantity { get; set; }
+        public double PurchasePrice { get; set; }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            return isValid;
+        }
+
+        public OrderItem Retrieve(int orderItemId)
+        {
+            return new OrderItem();
+        }
+
+        public List<OrderItem> Retrieve()
+        {
+            return new List<OrderItem>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+    }
+}
