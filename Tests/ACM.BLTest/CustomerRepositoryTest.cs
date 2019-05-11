85197a9b8a2a4d714322d7dc6ee6cc4c0d8600b8
Rakesh Baruah <rbaruah@gmail.com>
2019-05-11 01:17:15 -0500

    Created unit test class for customerRepository

---
+++ Tests/ACM.BLTest/CustomerRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerRepositoryTest
+    {
+        [TestMethod]
+        public void RetrieveValid()
+        {
+            //-- Arrange
+            var customerRepository = new CustomerRepository();
+            var expected = new Customer(1)
+            {                
+                EmailAddress = "fbaggins@hobbiton.me",
+                FirstName = "Frodo",
+                LastName = "Baggins"
+            };
+
+            //-- Act
+            var actual = customerRepository.Retrieve(1);
+
+
+            //-- Assert
+            Assert.AreEqual(expected.FullName, actual.FullName);
+        }
+    }
+}
